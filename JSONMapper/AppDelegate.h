890085e744b76978c4ff76707ebb88ef8a8dbd73
Mahadevan Sreenivasan <mahadevan_sv@yahoo.com>
2014-04-09 21:07:32 +0530

    Initial Commit

---
+++ JSONMapper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSONMapper
+//
+//  Created by Mahadevan Sreenivasan on 09/04/14.
+//  Copyright (c) 2014 Mahadevan Sreenivasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSONMapperTests/JSONMapperTests.m
@@ -0,0 +1,34 @@
+//
+//  JSONMapperTests.m
+//  JSONMapperTests
+//
+//  Created by Mahadevan Sreenivasan on 09/04/14.
+//  Copyright (c) 2014 Mahadevan Sreenivasan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSONMapperTests : XCTestCase
+
+@end
+
+@implementation JSONMapperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
